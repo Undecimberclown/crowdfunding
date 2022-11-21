7a01dcd72317e38b5606a4123b1d5d06a2ee18ab
Undecimberclown <gka8847@naver.com>
2022-11-21 12:50:45 +0900

    PaymentDTO

---
+++ src/main/java/com/team2/crowdfunding/model/PaymentDTO.java
@@ -0,0 +1,11 @@
+package com.team2.crowdfunding.model;
+
+import lombok.Data;
+
+@Data
+public class PaymentDTO {
+    private int id;
+    private int user_id;
+    private int commodity_id;
+    private int point;
+}
