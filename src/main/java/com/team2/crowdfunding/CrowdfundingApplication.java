5b4ba6087b94546863c0794e13214bcbded05346
Undecimberclown <gka8847@naver.com>
2022-11-21 16:09:32 +0900

    Revert "no message"
    This reverts commit 7a7413a09f07a458481fc6b86797ea233cd4ef4b.

--- src/main/java/com/team2/crowdfunding/CrowdfundingApplication.java
+++ src/main/java/com/team2/crowdfunding/CrowdfundingApplication.java
@@ -2,9 +2,8 @@ package com.team2.crowdfunding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class CrowdfundingApplication {
 
 	public static void main(String[] args) {
