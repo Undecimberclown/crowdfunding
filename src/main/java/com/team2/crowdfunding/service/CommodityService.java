fc5eb3c340db6f9d2c21db8a867ba20e8aa207c0
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-21 16:44:29 +0900

    selectAll 추가

--- src/main/java/com/team2/crowdfunding/service/CommodityService.java
+++ src/main/java/com/team2/crowdfunding/service/CommodityService.java
@@ -4,10 +4,18 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class CommodityService {
     @Autowired
     private SqlSession session;
 
     private final String NAMESPACE = "CommodityMapper";
+
+    public List<HashMap> selectAll(){
+        return session.selectList(NAMESPACE + ".selectAll");
+    }
 }
