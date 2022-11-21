065d37b200130e23d4d20783406b56f55e51ae8e
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-21 17:49:28 +0900

    commodity selectAll 수정

--- src/main/java/com/team2/crowdfunding/service/CommodityService.java
+++ src/main/java/com/team2/crowdfunding/service/CommodityService.java
@@ -1,5 +1,6 @@
 package com.team2.crowdfunding.service;
 
+import com.team2.crowdfunding.model.CommodityDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,11 @@ public class CommodityService {
 
     private final String NAMESPACE = "CommodityMapper";
 
-    public List<HashMap> selectAll(){
-        return session.selectList(NAMESPACE + ".selectAll");
+    public List<HashMap> selectAll(int id){
+        return session.selectList(NAMESPACE + ".selectAll", id);
+    }
+
+    public void insert(CommodityDTO commodityDTO){
+        session.insert(NAMESPACE + ".insert", commodityDTO);
     }
 }
