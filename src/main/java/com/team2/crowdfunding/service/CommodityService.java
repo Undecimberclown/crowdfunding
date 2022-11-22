13dc1ef98a967f0f7b0713b7987661435524f9a3
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-22 11:42:59 +0900

    commodity 컨트롤러, 서비스, 매퍼 수정

--- src/main/java/com/team2/crowdfunding/service/CommodityService.java
+++ src/main/java/com/team2/crowdfunding/service/CommodityService.java
@@ -20,7 +20,19 @@ public class CommodityService {
         return session.selectList(NAMESPACE + ".selectAll", id);
     }
 
+    public CommodityDTO selectOne(int id){
+        return session.selectOne(NAMESPACE + ".selectOne", id);
+    }
+
     public void insert(CommodityDTO commodityDTO){
         session.insert(NAMESPACE + ".insert", commodityDTO);
+    }
+
+    public void update(CommodityDTO commodityDTO){
+        session.update(NAMESPACE + ".update", commodityDTO);
+    }
+
+    public void delete(int id){
+        session.delete(NAMESPACE + ".delete", id);
     }
 }
