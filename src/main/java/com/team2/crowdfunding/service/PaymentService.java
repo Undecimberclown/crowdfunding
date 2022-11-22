a532aaf369b99abdf18e1e4c27aa823d8bc81479
yonghyeon123 <mnbvcx0229@gmail.com>
2022-11-22 13:18:11 +0900

    payment fill, pay 추가

--- src/main/java/com/team2/crowdfunding/service/PaymentService.java
+++ src/main/java/com/team2/crowdfunding/service/PaymentService.java
@@ -4,10 +4,21 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class PaymentService {
     @Autowired
     private SqlSession session;
 
     private final String NAMESPACE = "PaymentMapper";
+
+    public void fillPoint(Map<String, Integer> map){
+        session.update(NAMESPACE + "fillPoint", map);
+    }
+
+    public void payPoint(Map<String, Integer> map){
+        session.update(NAMESPACE + ".payPoint", map);
+    }
 }
