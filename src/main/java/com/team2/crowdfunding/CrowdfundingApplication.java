b521396f2cc5a68e6930d2bb0ed8b40c702bf206
Undecimberclown <gka8847@naver.com>
2022-11-28 17:15:14 +0900

    db 보안화

--- src/main/java/com/team2/crowdfunding/CrowdfundingApplication.java
+++ src/main/java/com/team2/crowdfunding/CrowdfundingApplication.java
@@ -3,10 +3,18 @@ package com.team2.crowdfunding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class CrowdfundingApplication {
+public class CrowdfundingApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(CrowdfundingApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CrowdfundingApplication.class, args);
 	}
