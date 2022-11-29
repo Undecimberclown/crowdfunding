90eb6558cead3f1a0c9f4f59a15c22068fd13712
Undecimberclown <gka8847@naver.com>
2022-11-29 16:10:02 +0900

    문법 수정

--- src/main/java/com/team2/crowdfunding/controller/ProjectController.java
+++ src/main/java/com/team2/crowdfunding/controller/ProjectController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/project/")
-                public class ProjectController {
+    public class ProjectController {
     @Autowired
     ProjectService projectService;
 
