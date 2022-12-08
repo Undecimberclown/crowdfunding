4f3cda1cc9b6ee4f1647351fe02db126fd7cc664
parkhyojin <gywlsdud05@gmail.com>
2022-12-08 10:45:49 +0900

    no message

--- src/main/java/com/team2/crowdfunding/service/BoardService.java
+++ src/main/java/com/team2/crowdfunding/service/BoardService.java
@@ -14,13 +14,22 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    //글 작성
     public void write(Project project){
 
         boardRepository.save(project);
     }
 
+    // 게시글 리스트
+
     public List<Project> boardList(){
         return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기
+    public Project boardView(Integer id) {
+
+        return boardRepository.findById(id).get() ;
     }
 
 }
