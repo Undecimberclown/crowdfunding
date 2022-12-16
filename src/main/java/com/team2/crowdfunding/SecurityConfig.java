07ddff82d7cc51d004bfe3eeaf33459c7e5f79eb
yonghyeon123 <mnbvcx0229@gmail.com>
2022-12-16 18:27:18 +0900

    Security 로그아웃 추가

--- src/main/java/com/team2/crowdfunding/SecurityConfig.java
+++ src/main/java/com/team2/crowdfunding/SecurityConfig.java
@@ -42,6 +42,10 @@ public class SecurityConfig {
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
                 .defaultSuccessUrl("/")
                 .and()
+                .logout()
+                .logoutUrl("/logOut")
+                .logoutSuccessUrl("/")
+                .and()
                 .oauth2Login()
                 .loginPage("/loginPage")
                 .userInfoEndpoint()
