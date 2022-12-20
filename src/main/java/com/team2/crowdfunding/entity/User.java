757ca8fc6fa1319bce4fdc51228dae7fd69b4145
yonghyeon123 <mnbvcx0229@gmail.com>
2022-12-20 13:19:04 +0900

    point 추가

--- src/main/java/com/team2/crowdfunding/entity/User.java
+++ src/main/java/com/team2/crowdfunding/entity/User.java
@@ -27,13 +27,15 @@ public class User {
     private String provider;
     private String provider_id;
 
+    private int point;
+
 //    @CreationTimestamp // INSERT 쿼리 시 현재 시간으로 생성
 //    private Timestamp create_date;
 //    // 회원가입 날짜
 
 
     @Builder
-    public User( String nickname, String username, String email, String password, String role, String provider, String provider_id) {
+    public User( String nickname, String username, String email, String password, String role, String provider, String provider_id, int point) {
 
         this.nickname = nickname;
         this.username = username;
@@ -42,5 +44,6 @@ public class User {
         this.email = email;
         this.provider = provider;
         this.provider_id = provider_id;
+        this.point = point;
     }
 }
